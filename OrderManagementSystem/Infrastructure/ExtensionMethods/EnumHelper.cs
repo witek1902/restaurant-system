443a6e08dc4d3f89e8c7d7bd2bdde746e11751fb
Robert Witkowski <robert.witkowski@altkom.pl>
2018-02-01 09:00:46 +0100

    Add extensions method.

---
+++ OrderManagementSystem/Infrastructure/ExtensionMethods/EnumHelper.cs
@@ -0,0 +1,25 @@
+﻿namespace OrderManagementSystem.Infrastructure.ExtensionMethods
+{
+    using System;
+    using System.ComponentModel.DataAnnotations;
+    using System.Linq;
+    using System.Reflection;
+
+    public static class EnumHelper
+    {
+        /// <summary>
+        /// DisplayName dla enuma
+        /// </summary>
+        /// <param name="enumValue"></param>
+        /// <returns></returns>
+        public static string GetDisplayName(this Enum enumValue)
+        {
+            return enumValue.GetType()
+                            .GetMember(enumValue.ToString())
+                            .First()
+                            .GetCustomAttribute<DisplayAttribute>()
+                            .GetName();
+        }
+
+    }
+}
---
+++ OrderManagementSystem/Infrastructure/ExtensionMethods/SessionHelper.cs
@@ -0,0 +1,16 @@
+﻿namespace OrderManagementSystem.Infrastructure.ExtensionMethods
+{
+    using NHibernate;
+
+    public static class SessionHelper
+    {
+        public static void Delete<TEntity>(this ISession session, object id)
+        {
+            var queryString = string.Format("delete {0} where id = :id",
+                                            typeof(TEntity));
+            session.CreateQuery(queryString)
+                   .SetParameter("id", id)
+                   .ExecuteUpdate();
+        }
+    }
+}
