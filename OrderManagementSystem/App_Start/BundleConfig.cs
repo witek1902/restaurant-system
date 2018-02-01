87495c6b4f61a5c71ae611f40c4a3230c98c70b3
Robert Witkowski <robert.witkowski@altkom.pl>
2018-02-01 08:48:20 +0100

    Add project main configuration files.

---
+++ OrderManagementSystem/App_Start/BundleConfig.cs
@@ -0,0 +1,41 @@
+﻿namespace OrderManagementSystem
+{
+    using System.Collections.Generic;
+    using System.Web.Optimization;
+
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            var scriptsBundle = new ScriptBundle("~/bundles/all") { Orderer = new AsIsBundleOrderer() };
+            scriptsBundle
+                        .Include(
+                            "~/Scripts/jquery-{version}.js",
+                            "~/Scripts/bootstrap.js",
+                            "~/Scripts/respond.js",
+                            "~/Scripts/ekko-lightbox.js",
+                            "~/Scripts/star-rating.min.js",
+                            "~/Scripts/modernizr-*",
+                            "~/Scripts/jquery.validate.js",
+                            "~/Scripts/jquery.validate.unobtrusive.js")
+                        .IncludeDirectory("~/Scripts/Application", "*.js", searchSubdirectories: false);
+
+            bundles.Add(scriptsBundle);
+
+            bundles.Add(new StyleBundle("~/Content/css")
+                .Include(
+                    "~/Content/bootstrap.css",
+                    "~/Content/star-rating.min.css",
+                    "~/Content/ekko-lightbox.css",
+                    "~/Content/Site.css"));
+        }
+
+        public class AsIsBundleOrderer : IBundleOrderer
+        {
+            public IEnumerable<BundleFile> OrderFiles(BundleContext context, IEnumerable<BundleFile> files)
+            {
+                return files;
+            }
+        }
+    }
+}
---
+++ OrderManagementSystem/Global.asax.cs
@@ -0,0 +1,38 @@
+﻿namespace OrderManagementSystem
+{
+    using Castle.MicroKernel.Registration;
+    using Castle.Windsor;
+    using Castle.Windsor.Installer;
+    using Infrastructure.IoC;
+    using System.Web.Mvc;
+    using System.Web.Optimization;
+    using System.Web.Routing;
+    using System.Web;
+
+    public class MvcApplication : HttpApplication
+    {
+        private static readonly IWindsorContainer container = new WindsorContainer();
+
+        public static IWindsorContainer Container => container;
+
+        protected void Application_Start()
+        {
+            InitWindsor();
+            ControllerBuilder.Current.SetControllerFactory(new WindsorControllerFactory(container));
+
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            //Uncomment if you want add NHibernateProfiler
+            //HibernatingRhinos.Profiler.Appender.NHibernate.NHibernateProfiler.Initialize();
+        }
+
+        private void InitWindsor()
+        {
+            Container.Register(Component.For<IWindsorContainer>().Instance(Container).LifestyleSingleton());
+            Container.Install(FromAssembly.This());
+        }
+    }
+}
